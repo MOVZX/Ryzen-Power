3bab61acda2c836fce328e7530c8cc120b584b06
MOVZX <movzx@yahoo.com>
2024-09-16 09:38:03 +0700

    build: Updates
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-gcc -o ryzen ryzen.c
-gcc -o cpuf cpuf.c
-gcc -o sens sens.c
+gcc -o ryzen ryzen.c -lm
+gcc -o cpuf cpuf.c -lm
+gcc -o sens sens.c -lm
+gcc -o powerusage powerusage.c -lm
