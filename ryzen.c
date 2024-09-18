5ed39f21cac0f2bc43c62b21fe4edbd8ffac30d1
MOVZX <movzx@yahoo.com>
2024-09-18 18:06:37 +0700

    ryzen: Simplify
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- ryzen.c
+++ ryzen.c
@@ -1,111 +1,79 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h> // For usleep function
-#include <sys/time.h> // For gettimeofday function
-#include <stdint.h> // For fixed-width integer types
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
 
-#define RAPL_FILE_PATH "/sys/class/powercap/intel-rapl:0/energy_uj"
-
+#define RAPL_PATH "/sys/class/powercap/intel-rapl:0/energy_uj"
 #define USEC 1000000
 
-// Function to get the current energy consumption in microjoules
-int64_t get_cpuConsumptionUJoules()
+static int64_t last_read_time = 0;
+static int64_t cached_consumption = -1;
+
+int64_t get_monotonicTimeUSec()
 {
-    int64_t consumption = -1;
-    FILE *file = fopen(RAPL_FILE_PATH, "r");
+    struct timespec time;
 
-    if (file == NULL)
-    {
-        perror("Error opening RAPL energy file");
+    clock_gettime(CLOCK_MONOTONIC, &time);
 
-        return -1;
-    }
-
-    if (fscanf(file, "%lld", &consumption) != 1)
-    {
-        perror("Error reading energy consumption");
-
-        consumption = -1;
-    }
-
-    fclose(file);
-
-    return consumption;
+    return time.tv_sec * USEC + time.tv_nsec / 1000;
 }
 
-// Function to get the current time in microseconds
-int64_t get_currentTimeUSec()
+int64_t get_cpuConsumptionUJoules()
 {
-    struct timeval tv;
+    int64_t current_time = get_monotonicTimeUSec();
 
-    if (gettimeofday(&tv, NULL) != 0)
+    if (current_time - last_read_time >= USEC)
     {
-        perror("Error getting current time");
+        FILE *file = fopen(RAPL_PATH, "r");
 
-        return -1;
+        if (file != NULL)
+        {
+            if (fscanf(file, "%lld", &cached_consumption) != 1)
+                cached_consumption = -1;
+
+            fclose(file);
+        }
+        else
+            perror("Failed to open RAPL path");
+
+        last_read_time = current_time;
     }
 
-    return ((int64_t)tv.tv_sec * USEC) + tv.tv_usec;
+    return cached_consumption;
+}
+
+float get_cpuConsumptionWatts()
+{
+    static int64_t previous_usage = -1;
+    static int64_t previous_timestamp = 0;
+
+    int64_t current_timestamp = get_monotonicTimeUSec();
+    int64_t current_usage = get_cpuConsumptionUJoules();
+
+    if (current_usage < 0 || previous_timestamp == current_timestamp)
+        return 0;
+
+    if (previous_usage < 0)
+    {
+        previous_usage = current_usage;
+        previous_timestamp = current_timestamp;
+
+        usleep(USEC);
+
+        current_timestamp = get_monotonicTimeUSec();
+        current_usage = get_cpuConsumptionUJoules();
+    }
+
+    float watts = (float)(current_usage - previous_usage) / (float)(current_timestamp - previous_timestamp);
+    previous_timestamp = current_timestamp;
+    previous_usage = current_usage;
+
+    return watts;
 }
 
 int main()
 {
-    int64_t initial_usage = get_cpuConsumptionUJoules();
-    int64_t initial_time = get_currentTimeUSec();
-
-    if (initial_usage == -1 || initial_time == -1)
-    {
-        fprintf(stderr, "Failed to read initial CPU consumption or time data.\n");
-
-        return 1;
-    }
-
-    // Short delay to allow measurable change
-    usleep(USEC); // Sleep for 1 second
-
-    int64_t final_usage = get_cpuConsumptionUJoules();
-    int64_t final_time = get_currentTimeUSec();
-
-    if (final_usage == -1 || final_time == -1)
-    {
-        fprintf(stderr, "Failed to read final CPU consumption or time data.\n");
-
-        return 1;
-    }
-
-    // Calculate the time difference and energy difference
-    if (final_time < initial_time)
-    {
-        fprintf(stderr, "Time went backwards.\n");
-
-        return 1;
-    }
-
-    int64_t time_diff_usec = final_time - initial_time;
-    int64_t energy_diff_uj = final_usage - initial_usage;
-
-    if (time_diff_usec <= 0)
-    {
-        fprintf(stderr, "Invalid time difference.\n");
-
-        return 1;
-    }
-
-    if (energy_diff_uj < 0)
-    {
-        fprintf(stderr, "Energy consumption decreased.\n");
-
-        return 1;
-    }
-
-    // Convert time difference to seconds
-    double time_diff_sec = time_diff_usec / USEC;
-
-    // Calculate power in watts
-    double watts = energy_diff_uj / (time_diff_sec * USEC);
-
-    // Print the result
-    printf("%.2lf\n", watts);
+    printf("%.2f\n", get_cpuConsumptionWatts());
 
     return 0;
 }
