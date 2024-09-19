49a1b42e5c22d88bd52630ca6452b3287ad14b2b
MOVZX <movzx@yahoo.com>
2024-09-19 10:12:21 +0700

    powerusage: Add DRAM & GPU usage
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- powerusage.c
+++ powerusage.c
@@ -11,6 +11,32 @@
 #define RAPL_FILE_PATH "/sys/class/powercap/intel-rapl:0/energy_uj"
 #define MAX_PROCESSES 100
 #define MAX_NAME_LENGTH 256
+#define USEC 1000000
+#define KILO 1000
+
+float get_memory_usage()
+{
+    FILE *file = fopen("/proc/meminfo", "r");
+
+    if (file == NULL)
+    {
+        perror("fopen");
+
+        exit(EXIT_FAILURE);
+    }
+
+    long total_memory = 0;
+    long free_memory = 0;
+
+    fscanf(file, "MemTotal: %ld kB\n", &total_memory);
+    fscanf(file, "MemFree: %ld kB\n", &free_memory);
+
+    fclose(file);
+
+    long used_memory = total_memory - free_memory;
+    // *used_memory_percent = ((float)used_memory / total_memory) * 100.0f; -- persen
+    return (float)used_memory / USEC;
+}
 
 int64_t get_cpuConsumptionUJoules()
 {
@@ -21,7 +47,8 @@ int64_t get_cpuConsumptionUJoules()
     {
         perror("Error reading energy consumption!");
 
-        if (file) fclose(file);
+        if (file)
+            fclose(file);
 
         return -1;
     }
@@ -42,7 +69,7 @@ int64_t get_currentTimeUSec()
         return -1;
     }
 
-    return ((int64_t)tv.tv_sec * 1000000) + tv.tv_usec;
+    return ((int64_t)tv.tv_sec * USEC) + tv.tv_usec;
 }
 
 float calculate_cpu_power()
@@ -50,16 +77,18 @@ float calculate_cpu_power()
     int64_t initial_usage = get_cpuConsumptionUJoules();
     int64_t initial_time = get_currentTimeUSec();
 
-    if (initial_usage == -1 || initial_time == -1) return -1.0f;
+    if (initial_usage == -1 || initial_time == -1)
+        return -1.0f;
 
-    usleep(1000000);
+    usleep(USEC);
 
     int64_t final_usage = get_cpuConsumptionUJoules();
     int64_t final_time = get_currentTimeUSec();
 
-    if (final_usage == -1 || final_time == -1 || final_time <= initial_time || final_usage <= initial_usage) return -1.0f;
+    if (final_usage == -1 || final_time == -1 || final_time <= initial_time || final_usage <= initial_usage)
+        return -1.0f;
 
-    return (float)((final_usage - initial_usage) / ((final_time - initial_time) / 1000000.0 * 1e6));
+    return (float)((final_usage - initial_usage) / ((final_time - initial_time) / USEC * USEC));
 }
 
 char* execute_command(const char* command)
@@ -68,16 +97,17 @@ char* execute_command(const char* command)
 
     if (!fp) return NULL;
 
-    char* output = malloc(1024);
+    char* output = malloc(KILO);
 
     if (!output) return NULL;
 
-    if (!fgets(output, 1024, fp))
+    if (!fgets(output, KILO, fp))
     {
         free(output);
         output = NULL;
     }
-    else strtok(output, "\n");
+    else
+        strtok(output, "\n");
 
     pclose(fp);
 
@@ -88,11 +118,12 @@ bool is_process_running_native(const char* process_name)
 {
     DIR* dir = opendir("/proc");
 
-    if (!dir) return false;
+    if (!dir)
+        return false;
 
     struct dirent* entry;
 
-    char path[256], pname[256], buffer[256];
+    char path[MAX_NAME_LENGTH], pname[MAX_NAME_LENGTH], buffer[MAX_NAME_LENGTH];
 
     while ((entry = readdir(dir)))
     {
@@ -123,7 +154,8 @@ int load_process_names(const char* config_file, char process_list[MAX_PROCESSES]
 {
     FILE* fp = fopen(config_file, "r");
 
-    if (!fp) return -1;
+    if (!fp)
+        return -1;
 
     char line[MAX_NAME_LENGTH];
     int count = 0;
@@ -142,9 +174,10 @@ int load_process_names(const char* config_file, char process_list[MAX_PROCESSES]
 bool is_any_process_running(char process_list[MAX_PROCESSES][MAX_NAME_LENGTH], int process_count)
 {
     for (int i = 0; i < process_count; i++)
-        if (is_process_running_native(process_list[i])) return true;
+        if (is_process_running_native(process_list[i]))
+            return true;
 
-        return false;
+    return false;
 }
 
 void print_cpu_info()
@@ -152,9 +185,10 @@ void print_cpu_info()
     char* cpu_temperature1 = execute_command("sensors k10temp-pci-* | awk -F '[:°C]' '/Tctl:/ {print $2}'");
     char* cpu_temperature2 = execute_command("sensors k10temp-pci-* | awk -F '[:°C]' '/Tccd1:/ {print $2}'");
     float cpu_power = calculate_cpu_power();
+    float used_memory_gb = get_memory_usage();
 
-    if (cpu_temperature1 && cpu_temperature2)
-        printf("   %.0f °C      %.0f °C   󰚥 %.0f W\n", atof(cpu_temperature1), atof(cpu_temperature2), cpu_power);
+    if (cpu_temperature1 && cpu_temperature2 && used_memory_gb)
+        printf("   %.0f GB |    %.0f °C |    %.0f °C | 󰚥 %.0f W\n", used_memory_gb, atof(cpu_temperature1), atof(cpu_temperature2), cpu_power);
 
     free(cpu_temperature1);
     free(cpu_temperature2);
@@ -162,14 +196,16 @@ void print_cpu_info()
 
 void print_gpu_info()
 {
+    char* gpu_usage = execute_command("rocm-smi -d 0 --showuse | awk '/GPU use \\(%\\)/ {print $NF}'");
     char* gpu_temperature1 = execute_command("rocm-smi -t | awk '/Temperature \\(Sensor edge\\) \\(C\\):/ {print $NF}'");
     char* gpu_temperature2 = execute_command("rocm-smi -t | awk '/Temperature \\(Sensor junction\\) \\(C\\):/ {print $NF}'");
     char* gpu_temperature3 = execute_command("rocm-smi -t | awk '/Temperature \\(Sensor memory\\) \\(C\\):/ {print $NF}'");
     char* gpu_power = execute_command("rocm-smi -P | awk '/Average Graphics Package Power \\(W\\):/ {print $NF}'");
 
-    if (gpu_temperature1 && gpu_temperature2 && gpu_temperature3 && gpu_power)
-        printf("   %.0f °C      %.0f °C      %.0f °C   󰚥 %.0f W\n", atof(gpu_temperature1), atof(gpu_temperature2), atof(gpu_temperature3), atof(gpu_power));
+    if (gpu_temperature1 && gpu_temperature2 && gpu_temperature3 && gpu_usage)
+        printf("   %.0f \% |    %.0f °C |    %.0f °C |    %.0f °C | 󰚥 %.0f W\n", atof(gpu_usage), atof(gpu_temperature1), atof(gpu_temperature2), atof(gpu_temperature3), atof(gpu_power));
 
+    free(gpu_usage);
     free(gpu_temperature1);
     free(gpu_temperature2);
     free(gpu_temperature3);
@@ -180,7 +216,7 @@ int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        fprintf(stderr, "Sintaks: %s CONFIG CPU_GPU, Contoh: %s ~/.config/daftar_hitam.conf cpu\n", argv[0]);
+        fprintf(stderr, "Sintaks: powerusage CONFIG CPU_GPU, Contoh: powerusage ~/.config/daftar_hitam.conf cpu\n", argv[0]);
 
         return 1;
     }
@@ -188,11 +224,15 @@ int main(int argc, char* argv[])
     char process_list[MAX_PROCESSES][MAX_NAME_LENGTH];
     int process_count = load_process_names(argv[1], process_list);
 
-    if (process_count == -1 || is_any_process_running(process_list, process_count)) return 1;
+    if (process_count == -1 || is_any_process_running(process_list, process_count))
+        return 1;
 
-    if (strcmp(argv[2], "cpu") == 0) print_cpu_info();
-    else if (strcmp(argv[2], "gpu") == 0) print_gpu_info();
-    else fprintf(stderr, "Invalid mode: %s. Use 'cpu' or 'gpu'.\n", argv[2]);
+    if (strcmp(argv[2], "cpu") == 0)
+        print_cpu_info();
+    else if (strcmp(argv[2], "gpu") == 0)
+        print_gpu_info();
+    else
+        fprintf(stderr, "Salah mode: %s. Gunakan 'cpu' atau 'gpu'.\n", argv[2]);
 
     return 0;
 }
