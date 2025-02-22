e71cd46a5c00f16eb06f5dacc905f9d56e7d6595
MOVZX <movzx@yahoo.com>
2025-02-22 16:56:25 +0700

    Update Build Script
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcc -o ryzen ryzen.c -lm
-gcc -o cpuf cpuf.c -lm
-gcc -o sens sens.c -lm
-gcc -o powerusage powerusage.c -lm
+gcc -O2 -o ryzen ryzen.c -lm
+gcc -O2 -o cpuf cpuf.c -lm
+gcc -O2 -o sens sens.c -lm
+gcc -O2 -o powerusage powerusage.c -lm
