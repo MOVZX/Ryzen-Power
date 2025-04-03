f097b1f3210cf463748e470633b9db9ef55ac5a4
MOVZX <movzx@yahoo.com>
2025-04-03 11:37:27 +0700

    Update build script
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcc -O2 -o ryzen ryzen.c -lm
-gcc -O2 -o cpuf cpuf.c -lm
-gcc -O2 -o sens sens.c -lm
-gcc -O2 -o powerusage powerusage.c -lm
+gcc -O3 -o ryzen ryzen.c -lm
+gcc -O3 -o cpuf cpuf.c -lm
+gcc -O3 -o sens sens.c -lm
+gcc -O3 -Wall -Werror -Wextra -Wshadow -Wpointer-arith -Wstrict-prototypes -Wmissing-prototypes -Wold-style-definition -Wvla -I/opt/cuda/include -o powerusage powerusage.c -lpci -lnvidia-ml -lm
